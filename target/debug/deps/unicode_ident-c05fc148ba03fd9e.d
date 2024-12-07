9e041e499d4fe549f1098f50ad4ddeb52a7a348b
Shvngishrma <shvngishrmaa@gmail.com>
2024-12-07 11:08:00 +0530

    basic figuring out stuff

---
+++ target/debug/deps/unicode_ident-c05fc148ba03fd9e.d
@@ -0,0 +1,6 @@
+/Users/prvjhnwr/projects/guessing_game/target/debug/deps/libunicode_ident-c05fc148ba03fd9e.rmeta: /Users/prvjhnwr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /Users/prvjhnwr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
+
+/Users/prvjhnwr/projects/guessing_game/target/debug/deps/unicode_ident-c05fc148ba03fd9e.d: /Users/prvjhnwr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /Users/prvjhnwr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
+
+/Users/prvjhnwr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs:
+/Users/prvjhnwr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs:
